d8c008469518455da5e28c28243347ca34c355da
KarolinaKrysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2021-09-30 23:19:19 +0200

    Added js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,68 @@
 "use strict";
 
-console.log("Ready 🎉");
+// =========== character functionality =========== //
+/*
+global variables: _characters
+*/
+let _characters = [];
+
+/*
+Fetches json data from the file characters.json
+*/
+fetch("json/data.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    _characters = data;
+    appendcharacters(_characters);
+  });
+
+function appendcharacters(characters) {
+  let htmlTemplate = "";
+  for (let character of characters) {
+    htmlTemplate += /*html*/ `
+      <article class="${character.status}">
+        <img src="${character.img}">
+        <h2>${character.name}</h2>
+        <h3>${character.game}</h3>
+      </article>
+    `;
+  }
+  document.querySelector("#characters-container").innerHTML = htmlTemplate;
+}
+
+function addNewcharacter() {
+  let name = document.querySelector("#name").value;
+  let game = document.querySelector("#game").value;
+  let img = document.querySelector("#img").value;
+
+  if (game && name && img) {
+    _characters.push({
+      game,
+      name,
+      img,
+    });
+
+    appendcharacters(_characters);
+    navigateTo("characters");
+    document.querySelector("#game").value = "";
+  } else {
+    alert("Please fill out all fields");
+  }
+}
+
+function search(value) {
+  let searchQuery = value.toLowerCase();
+  let filteredcharacters = [];
+  for (let character of _characters) {
+    let name = character.name.toLowerCase();
+    let game = character.game.toLowerCase();
+    if (name.includes(searchQuery) || game.includes(searchQuery)) {
+      filteredcharacters.push(character);
+    }
+  }
+  console.log(filteredcharacters);
+  appendcharacters(filteredcharacters);
+}
