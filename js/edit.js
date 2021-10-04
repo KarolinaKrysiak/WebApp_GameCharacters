17badf443a2fd59630d8c7796375278846dff458
KarolinaKrysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2021-10-04 16:53:17 +0200

    Update

--- js/edit.js
+++ js/edit.js
@@ -1,38 +1,19 @@
-"use strict";
-
-// =========== character functionality =========== //
 /*
-global variables: _favCharacters _selectedcharacterId
+Shows random detailed view
 */
-let _favCharacters = [];
 
-/*
-Fetches json data from the file favCharacters.json
-*/
-async function fetchNewData() {
-  const response = await fetch('json/characters.json');
-  const data = await response.json();
-  _favCharacters = data;
-  console.log(_favCharacters);
-  appendFavCharacters(_favCharacters);
-  showLoader(false);
-}
-
-fetchNewData();
-
-function appendFavCharacters(favCharacters) {
-  let htmlTemplate = "";
-  for (let character of favCharacters) {
-    htmlTemplate += /*html*/`
-      <article class="${character.status}">
-        <article onclick="showDetailView(${character.id})">
-          <img src="${character.img}">
-          <h2>${character.name}</h2>
-          <h3>${character.game}</h3>
-        </article>
-       
-      </article>
-    `;
-  }
-  document.querySelector('#fav-characters-container').innerHTML = htmlTemplate;
+function showRandomDetailView(id) {
+  const characterToShow = _characters.find(character => character.id === id);
+  navigateTo("detail-view");
+  document.querySelector("#detail-view .title").innerHTML = characterToShow.name;
+  document.querySelector("#random-detail-view-container").innerHTML = /*html*/`
+    <img src="${characterToShow.img}">
+    <article>
+      <h2>${characterToShow.name}</h2>
+      <h3>${characterToShow.game}</h3>
+      <p>Gender: ${characterToShow.gender}</p>
+      <p>Race: ${characterToShow.race}</p>
+      <p class="description">${characterToShow.description}</p>
+    </article>
+  `;
 }
--- js/main.js
+++ js/main.js
@@ -184,11 +184,35 @@ function orderByname() {
   appendCharacters(_characters);
 }
 
+/*
+Shows detailed view
+*/
+
 function showDetailView(id) {
   const characterToShow = _characters.find(character => character.id === id);
   navigateTo("detail-view");
   document.querySelector("#detail-view .title").innerHTML = characterToShow.name;
   document.querySelector("#detail-view-container").innerHTML = /*html*/`
+    <img src="${characterToShow.img}">
+    <article>
+      <h2>${characterToShow.name}</h2>
+      <h3>${characterToShow.game}</h3>
+      <p>Gender: ${characterToShow.gender}</p>
+      <p>Race: ${characterToShow.race}</p>
+      <p class="description">${characterToShow.description}</p>
+    </article>
+  `;
+}
+
+/*
+Shows random detailed view
+*/
+
+function showRandomDetailView(id) {
+  const characterToShow = _characters.find(character => character.id === id);
+  navigateTo("random");
+  document.querySelector("#detail-view .title").innerHTML = characterToShow.name;
+  document.querySelector("#random-detail-view-container").innerHTML = /*html*/`
     <img src="${characterToShow.img}">
     <article>
       <h2>${characterToShow.name}</h2>
